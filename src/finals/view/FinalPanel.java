24f6d5948e4718c61c1c44180872f67f69dcb026
omatvejeva <98617225+omatvejeva@users.noreply.github.com>
2023-04-19 12:19:52 -0600

    started adding buttons and panels

--- src/finals/view/FinalPanel.java
+++ src/finals/view/FinalPanel.java
@@ -2,10 +2,13 @@ package finals.view;
 
 import finals.controller.Controller; 
 
-import javax.swing.JPanel; 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JButton; 
 import javax.swing.SpringLayout; 
+import javax.swing.JTextArea; 
 
+import java.awt.GridLayout; 
 import java.awt.Color; 
 
 
@@ -15,6 +18,13 @@ public class FinalPanel extends JPanel
 	
 	private SpringLayout layout; 
 	
+	private JPanel buttonPanel;
+	private JScrollPane dataPane; 
+	private JTextArea displayArea; 
+	private JButton cactiButton; 
+	private JButton succulentsButton; 
+	
+	
 	
 	
 	public FinalPanel(Controller app)
@@ -23,6 +33,12 @@ public class FinalPanel extends JPanel
 		this.app = app; 
 		
 		this.layout = new SpringLayout(); 
+		
+		this.buttonPanel = new JPanel (new GridLayout(0,2)); 
+		this.displayArea = new JTextArea(); 
+		this.dataPane = new JScrollPane(); 
+		this.cactiButton = new JButton("Cacti"); 
+		this.succulentsButton = new JButton("Succulents"); 
 		
 		setupPanel(); 
 		setupLayout(); 
